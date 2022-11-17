eca6746c08607a33b31d3b168491f711fcf08a7a
Antonio Marlon <antonio.pereira008@hotmail.com>
2022-11-17 12:57:19 -0300

    parei na aula 8

--- Api_basica/src/index.js
+++ Api_basica/src/index.js
@@ -1,8 +1,22 @@
 const http = require('http');
+const { type } = require('os');
+const routes = require('./routes');
 
 const server = http.createServer((request, response)=>{
-  response.writeHead(200,{ 'Content-Type': 'text/html' });
-  response.end('<h1>Hello word!!</h1>')
+
+  console.log(`Request Method: ${request.method} | Endpoint: ${request.url}`);
+
+  const route = routes.find((routObj) => (
+    routObj.endpoint === request.url && routObj.method === request.method
+  ));
+
+  if(route){
+    route.handler(request,response);
+  } else {
+    response.writeHead(404,{'Content-type': 'text/html' });
+    response.end(`Cannot ${request.method} ${request.url}`);
+  }
+ 
 });
 
 server.listen(3000, () => console.log('Sever rodando na porta 3000'));
